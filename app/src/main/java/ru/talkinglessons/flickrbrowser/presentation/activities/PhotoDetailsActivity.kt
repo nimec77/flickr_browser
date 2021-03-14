78058d514bfcd32e3503ffa8f03683283116beb6
comrade77 <nimec77@gmail.com>
2021-03-14 22:29:34 +0700

    Закончил №16

---
+++ app/src/main/java/ru/talkinglessons/flickrbrowser/presentation/activities/PhotoDetailsActivity.kt
@@ -0,0 +1,21 @@
+package ru.talkinglessons.flickrbrowser.presentation.activities
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import ru.talkinglessons.flickrbrowser.R
+
+class PhotoDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_details)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
