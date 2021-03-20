593bbe8ad687c164b8efe11d4e9cf4c0428486b7
comrade77 <nimec77@gmail.com>
2021-03-20 17:19:19 +0700

    Закончил №18

---
+++ app/src/main/java/ru/talkinglessons/flickrbrowser/presentation/adapters/FlickrRecyclerViewAdapter.kt
@@ -0,0 +1,50 @@
+package ru.talkinglessons.flickrbrowser.presentation.adapters
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import ru.talkinglessons.flickrbrowser.R
+import ru.talkinglessons.flickrbrowser.domain.entities.Photo
+
+class FlickrImageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    var thumbnail: ImageView = view.findViewById(R.id.thumbnail)
+    var title: TextView = view.findViewById(R.id.photo_title)
+}
+
+class FlickrRecyclerViewAdapter(private var photoList: List<Photo>) :
+    RecyclerView.Adapter<FlickrImageViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FlickrImageViewHolder {
+        Log.d(TAG, "onCreateViewHolder new view requested")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.browse, parent, false)
+
+        return FlickrImageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: FlickrImageViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        Log.d(TAG, "getItemCount called")
+        return if (photoList.isNotEmpty()) photoList.size else 0
+    }
+
+    fun loadNewData(newPhotos: List<Photo>) {
+        photoList = newPhotos;
+        notifyDataSetChanged();
+    }
+
+    fun getPhoto(position: Int): Photo? {
+        return if (photoList.isNotEmpty()) photoList[position] else null
+    }
+
+    companion object {
+        private const val TAG = "FlickrRecyclerViewAdapt"
+    }
+
+}
