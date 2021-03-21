313524f460de6a13475ee5301e9dbee0fad21028
comrade77 <nimec77@gmail.com>
2021-03-21 19:19:36 +0700

    Закончил №24

--- app/src/main/java/ru/talkinglessons/flickrbrowser/domain/entities/Photo.kt
+++ app/src/main/java/ru/talkinglessons/flickrbrowser/domain/entities/Photo.kt
@@ -1,7 +1,52 @@
 package ru.talkinglessons.flickrbrowser.domain.entities
 
-class Photo(val title: String, val author: String, val authorId: String, val link: String, val tags: String, val image: String) {
+import android.util.Log
+import java.io.IOException
+import java.io.ObjectStreamException
+import java.io.Serializable
+
+class Photo(
+    var title: String,
+    var author: String,
+    var authorId: String,
+    var link: String,
+    var tags: String,
+    var image: String
+) : Serializable {
     override fun toString(): String {
-        return "Photo(title='$title', author='$author', authorId='$authorId', link='$link', tags='$tags', image='$image'";
+        return "Photo(title='$title', author='$author', authorId='$authorId', link='$link', " +
+                "tags='$tags', image='$image'";
+    }
+
+    @Throws(IOException::class)
+    private fun writeObject(out: java.io.ObjectOutputStream) {
+        Log.d(TAG, "writeObject called")
+        out.writeUTF(title)
+        out.writeUTF(author)
+        out.writeUTF(authorId)
+        out.writeUTF(link)
+        out.writeUTF(tags)
+        out.writeUTF(image)
+    }
+
+    @Throws(IOException::class, ClassNotFoundException::class)
+    private fun readObject(inStream: java.io.ObjectInputStream) {
+        Log.d(TAG, "readObject called")
+        title = inStream.readUTF()
+        author = inStream.readUTF()
+        authorId = inStream.readUTF()
+        link = inStream.readUTF()
+        tags = inStream.readUTF()
+        image = inStream.readUTF()
+    }
+
+    @Throws(ObjectStreamException::class)
+    private fun readObjectNoData() {
+        Log.d(TAG, "readObjectNoData called")
+    }
+
+    companion object {
+        private const val TAG = "Photo"
+        private const val serialVersionUID = 1L
     }
 }
