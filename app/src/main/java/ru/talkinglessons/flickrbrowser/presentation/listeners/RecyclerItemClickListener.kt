c6fd62f85a6d3b8b48a57f00a5c8d18f9f0d8f5d
comrade77 <nimec77@gmail.com>
2021-03-21 16:52:33 +0700

    Закончил №22

--- app/src/main/java/ru/talkinglessons/flickrbrowser/presentation/listeners/RecyclerItemClickListener.kt
+++ app/src/main/java/ru/talkinglessons/flickrbrowser/presentation/listeners/RecyclerItemClickListener.kt
@@ -2,8 +2,10 @@ package ru.talkinglessons.flickrbrowser.presentation.listeners
 
 import android.content.Context
 import android.util.Log
+import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
+import androidx.core.view.GestureDetectorCompat
 import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerItemClickListener(
@@ -17,9 +19,28 @@ class RecyclerItemClickListener(
         fun onItemLongClick(view: View, position: Int)
     }
 
+    private val gestureDetector = GestureDetectorCompat(context, object: GestureDetector.SimpleOnGestureListener() {
+        override fun onSingleTapUp(e: MotionEvent): Boolean {
+            Log.d(TAG, "onSingleTapUp: starts")
+            val childView = recyclerView.findChildViewUnder(e.x, e.y)!!
+            Log.d(TAG, "onSingleTapUp calling listener.onItemClick")
+            listener.onItemClick(childView, recyclerView.getChildAdapterPosition(childView))
+            return true
+        }
+
+        override fun onLongPress(e: MotionEvent) {
+            Log.d(TAG, "onLongPress: starts")
+            val childView = recyclerView.findChildViewUnder(e.x, e.y)!!
+            Log.d(TAG, "onLongPress calling listener.onItemLongClick")
+            listener.onItemLongClick(childView, recyclerView.getChildAdapterPosition(childView))
+        }
+    })
+
     override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
         Log.d(TAG, "onInterceptTouchEvent: start $e")
-        return super.onInterceptTouchEvent(rv, e)
+        val result = gestureDetector.onTouchEvent(e);
+//        return super.onInterceptTouchEvent(rv, e)
+        return result
     }
 
     companion object {
