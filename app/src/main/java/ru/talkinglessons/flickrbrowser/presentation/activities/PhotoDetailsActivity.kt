8811a237cdd25b9ed2fca6e960ce929d5d232f9e
comrade77 <nimec77@gmail.com>
2021-03-21 19:38:35 +0700

    Закончил №25

--- app/src/main/java/ru/talkinglessons/flickrbrowser/presentation/activities/PhotoDetailsActivity.kt
+++ app/src/main/java/ru/talkinglessons/flickrbrowser/presentation/activities/PhotoDetailsActivity.kt
@@ -1,10 +1,10 @@
 package ru.talkinglessons.flickrbrowser.presentation.activities
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.content_photo_details.*
 import ru.talkinglessons.flickrbrowser.R
+import ru.talkinglessons.flickrbrowser.domain.entities.Photo
 
 class PhotoDetailsActivity : BaseActivity() {
 
@@ -12,5 +12,16 @@ class PhotoDetailsActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_details)
         activateToolbar(true)
+
+        val photo = intent.getSerializableExtra(PHOTO_TRANSFER) as Photo
+
+        photo_title.text = photo.title
+        photo_tags.text = photo.tags
+        photo_author.text = photo.author
+        Picasso.get()
+            .load(photo.link)
+            .error(R.drawable.placeholder)
+            .placeholder(R.drawable.placeholder)
+            .into(photo_image)
     }
 }
